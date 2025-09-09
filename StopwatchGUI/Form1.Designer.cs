7621862600deb96c886234516ca1adc9bed32870
Ali Razi <95105244+alirazi1992@users.noreply.github.com>
2025-09-09 10:07:41 +0330

    Add files via upload

---
+++ StopwatchGUI/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace StopwatchGUI
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblTime;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(20, 60);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 30);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(110, 60);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 30);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(200, 60);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 30);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Bold);
+            this.lblTime.Location = new System.Drawing.Point(20, 15);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(180, 32);
+            this.lblTime.TabIndex = 3;
+            this.lblTime.Text = "00:00:00.00";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(300, 120);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Stopwatch (Day 15)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ StopwatchGUI/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace StopwatchGUI
+{
+    public partial class Form1 : Form
+    {
+        private Stopwatch _stopwatch;
+        private Timer _timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _stopwatch = new Stopwatch();
+            _timer = new Timer();
+            _timer.Interval = 50; // update every 50ms
+            _timer.Tick += Timer_Tick;
+
+            lblTime.Text = "00:00:00.00";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            _stopwatch.Start();
+            _timer.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            _stopwatch.Stop();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            _stopwatch.Reset();
+            lblTime.Text = "00:00:00.00";
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan ts = _stopwatch.Elapsed;
+            lblTime.Text = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+        }
+    }
+}
